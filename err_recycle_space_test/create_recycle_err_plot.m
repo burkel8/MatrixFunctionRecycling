83e5d4f1129ce8303a14e7c1d8c2495c0de119fb
burkel8 <83296133+burkel8@users.noreply.github.com>
2022-06-20 22:39:51 +0100

    Added more results and in particular the accuracy of U test

--- err_recycle_space_test/create_recycle_err_plot.m
+++ err_recycle_space_test/create_recycle_err_plot.m
@@ -1,16 +1,30 @@
-load("err_recyclepace_smallLQCDsignm50k20numquad1000.mat")
-load("err_recyclepace_smallLQCDsignm50k20numquad1000matchanging.mat")
+load("eps0.mat");
+eps0 = err_recycle_space;
+load("eps0001.mat")
+eps0001 = err_recycle_space;
+load("eps001.mat")
+eps001 = err_recycle_space;
+load("eps01.mat");
+eps01 = err_recycle_space;
+load("eps1.mat");
+eps1 = err_recycle_space;
 
-num_systems = 15;
-xx = 1:num_systems+1;
+num_systems = 10;
+xx = 1:num_systems;
 
 
-semilogy(xx,rhschanging_err_recycle_space);
+semilogy(xx,eps0);
 hold on;
-semilogy(xx,matchanging_err_recycle_space);
+semilogy(xx,eps0001);
+hold on;
+semilogy(xx,eps001);
+hold on;
+semilogy(xx,eps01);
+hold on;
+semilogy(xx,eps1);
 hold off;
 grid on;
-title("Accuracy of $\mathcal{U}$ as an eigenvector approximation", 'Interpreter', 'latex');
-legend('QCD sign - rhs changing only', 'QCD sign - Matrix and rhs changing $\epsilon = 0.01$','interpreter','latex');
-xlabel('system index $i$', 'interpreter', 'latex');
-ylabel('fdf','interpreter','latex');
+title("QCD sign function - Accuracy of $\mathcal{U}$ as an eigenvector approximation", 'Interpreter', 'latex');
+legend('$\epsilon=0$', '$\epsilon = 0.001$',"$\epsilon = 0.01$", "$\epsilon=0.1$", "$\epsilon = 1$",'interpreter','latex');
+xlabel('problem index $i$', 'interpreter', 'latex');
+ylabel('$ \frac{\|\textbf{U} \textbf{D} - \textbf{A} \textbf{U}\|_{fro}}{\|\textbf{U}\|_{fro}} $','interpreter','latex');
