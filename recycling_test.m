a75a945aa7924502e1867903465c4f70cf269327
burkel8 <83296133+burkel8@users.noreply.github.com>
2022-06-20 19:39:20 +0100

    A few minor corrections from previous commit

--- recycling_test.m
+++ recycling_test.m
@@ -11,17 +11,17 @@ which_matrix = "smallLQCD";
 % -- Sign function ("sign")
 % -- log function ("log")
 % -- square root function ("sqrt")
-problem = 'inverse';
+problem = 'sign';
 
 
 %% Parameters of solve
 m = 50;  %Arnoldi cycle length
-k = 30;  %recycle space dimension
+k = 10;  %recycle space dimension
 N = 50;  %Parameter for Poisson and chemical potential matrix (value 
          %does not matter for other matrices)
-num_quad_points = 5000;   %number of quadrature points (add as many differnt points to this list)
-matrix_eps = 0.0001;  %parameter to determine how much the matrix changes.
-num_systems = 15;
+num_quad_points = 2000;   %number of quadrature points (add as many differnt points to this list)
+matrix_eps = 0.0;  %parameter to determine how much the matrix changes.
+num_systems = 1;
 %%%%%%%%%%%%%%    END USER INPUT HERE  %%%%%%%%%%%%%%%%%%%
 
 e1 = zeros(m,1);
@@ -31,7 +31,7 @@ err_arnoldi = zeros(1,num_systems);
 err_quad_arnoldi = zeros(1,num_systems);
 err_rFOM = zeros(1,num_systems);
 err_rGMRES = zeros(1,num_systems);
-err_recycle_space = zeros(num_systems);
+err_recycle_space = zeros(1,num_systems);
 
 [f_scalar, f_matrix] = return_function(problem);
 [S,n] = return_matrix(which_matrix,N);
@@ -110,8 +110,8 @@ hold on;
 semilogy(xx,err_rFOM/norm(x),'-o');
 hold off;
 
-title('sign($\textbf{A}$)\textbf{b} - approximation accuracy vs number of quadrature nodes','interpreter','latex')
-xlabel('System number','interpreter','latex');
+title('sign($\textbf{A}$)\textbf{b} - approximation error vs. problem index','interpreter','latex')
+xlabel('problem index','interpreter','latex');
 ylabel('$\| f(A)b - x_{m} \|_{fro}$','interpreter','latex');
 grid on;
 legend('Arnoldi','Arnoldi via quadrature','rFOM$^{2}$','interpreter','latex');
